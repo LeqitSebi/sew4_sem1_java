93506a35d1273efff83a2c29e540593fe7212a40
slanitsch <7225@htl.rennweg.at>
2020-09-16 09:58:05 +0200

    First test cases, only simple ones.

--- ue01_junit/Bezahlung.java
+++ ue01_junit/Bezahlung.java
@@ -6,10 +6,6 @@ import java.util.regex.Pattern;
 public class Bezahlung {
     private static final Pattern word = Pattern.compile("[A-z]+");
 
-    public static void main(String[] args) {
-        System.out.println(count("Ich mag SEW"));
-    }
-
     public static int count(String s){
         int wordcount = 0;
         String[] allwords = s.split(" ");
