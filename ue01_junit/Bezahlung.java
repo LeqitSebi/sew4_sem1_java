255695635709d235bcc4796744bc940cb688f443
slanitsch <7225@htl.rennweg.at>
2020-09-16 08:51:32 +0200

    First Version of wordcounter. no-html not implemented

--- ue01_junit/Bezahlung.java
+++ ue01_junit/Bezahlung.java
@@ -1,4 +1,24 @@
 package ue01_junit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Bezahlung {
+    private static final Pattern word = Pattern.compile("[A-z]+");
+
+    public static void main(String[] args) {
+        System.out.println(count("Ich mag SEW"));
+    }
+
+    public static int count(String s){
+        int wordcount = 0;
+        String[] allwords = s.split(" ");
+        for (String n:allwords) {
+            Matcher m = word.matcher(n);
+            if (m.matches()){
+                wordcount++;
+            }
+        }
+        return wordcount;
+    }
 }
