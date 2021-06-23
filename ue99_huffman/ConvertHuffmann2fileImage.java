28b606c41d38f15f7c0a5110f1e266f97eb55b37
slanitsch <7225@htl.rennweg.at>
2021-06-23 07:58:12 +0200

    Finished Huffman.

--- ue99_huffman/ConvertHuffmann2fileImage.java
+++ ue99_huffman/ConvertHuffmann2fileImage.java
@@ -9,25 +9,52 @@ import java.io.IOException;
 public class ConvertHuffmann2fileImage {
     /**
      * Wandelt eine Huffman-(RLE-) komprimierte Datei in eine normale Bild-Datei um.<br>
-     *     Erlaubte Formate: png, jpeg (Die Dateiendung der dest-Datei gibt das Dateiformat an.)
+     * Erlaubte Formate: png, jpeg (Die Dateiendung der dest-Datei gibt das Dateiformat an.)
      *
-     * @param src    die Huffman (und eventuell auch RLE) komprimiert Datei (Dateiname)
-     * @param dest   die zu erstellende Bild-Datei (Dateiname)
-     * @param isRLE  gibt an, ob die src-Datei auch RLE-komprimiert ist
+     * @param src   die Huffman (und eventuell auch RLE) komprimiert Datei (Dateiname)
+     * @param dest  die zu erstellende Bild-Datei (Dateiname)
+     * @param isRLE gibt an, ob die src-Datei auch RLE-komprimiert ist
      * @throws IOException
      */
     public static void convertHuffman2fileImage(String src, String dest, boolean isRLE) throws IOException {
         HuffmanImage hImg = new HuffmanImage(src);
-        NormalImage  nImg = new NormalImage(hImg.getWidth(), hImg.getHeight());
+        NormalImage nImg = new NormalImage(hImg.getWidth(), hImg.getHeight());
+        Huffman state = Huffman.START;
+        try {
+            if (isRLE) {
+                while (true) {
+                    if (state == Huffman.START) {
+                        nImg.setNColor(hImg.getNColor());
+                    }
+                    state = state.nextState(nImg, hImg.getBit());
+                }
+            }
+        } catch (Exception e) {
 
-        // TODO: Code ergaenzen
+        }
+        try {
+            while (true) {
+                state = state.nextState(nImg, hImg.getBit());
+            }
+        } catch (Exception e) {
+        }
 
         nImg.writeImageTo(dest);
     }
 
 
     public static void main(String[] args) throws IOException {
+        convertHuffman2fileImage("src/resources/a_01.huffman", "src/resources/a_01.png", false);
+        convertHuffman2fileImage("src/resources/a_01.huffman", "src/resources/a_01.jpeg", false);
 
+        convertHuffman2fileImage("src/resources/a_01.huffman.rle", "src/resources/a_01RLE.png", true);
+        convertHuffman2fileImage("src/resources/a_01.huffman.rle", "src/resources/a_01RLE.jpeg", true);
+
+        convertHuffman2fileImage("src/resources/b_01.huffman", "src/resources/b_01.png", false);
+        convertHuffman2fileImage("src/resources/b_01.huffman", "src/resources/b_01.jpeg", false);
+
+        convertHuffman2fileImage("src/resources/b_01.huffman.rle", "src/resources/b_01RLE.png", true);
+        convertHuffman2fileImage("src/resources/b_01.huffman.rle", "src/resources/b_01RLE.jpeg", true);
         System.out.println(ConvertHuffmann2fileImage.class.getSimpleName() + " finished");
     }
 }
